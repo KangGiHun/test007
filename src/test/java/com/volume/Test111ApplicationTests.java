fb46007622e473474f505f9274bb04e5ae80d9c7
SongLui <SongLui@DESKTOP-CS8O9MT>
2024-02-01 18:09:04 +0900

    feat:작업한 내용

---
+++ src/test/java/com/volume/Test111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test111ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
